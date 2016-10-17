abeff4c721c108b924f91fdaa8074cabb1c0001b
ehm731 <a-lamb@live.se>
2016-10-17 20:47:50 +0200

    Added test to CommandPwdChdirTest

--- src/test/java/se/inte/CommandPwdChdirTest.java
+++ src/test/java/se/inte/CommandPwdChdirTest.java
@@ -12,24 +12,37 @@ public class CommandPwdChdirTest {
 
     private Terminal terminal;
     private Folder homeFolder;
-    private Folder subfolder;
+    private Folder subFolder1;
+    private Folder subFolder2;
 
     @Before
     public void before(){
         terminal = new Terminal();
         homeFolder = new MockedFolder("homeFolder", null);
-        subfolder = new MockedFolder("subFolder", homeFolder);
+        subFolder1 = new MockedFolder("subFolder1", homeFolder);
+        subFolder2 = new MockedFolder("subFolder2", subFolder1);
     }
 
 
     @Test
-    public void PwdChdirTest(){
+    public void ChdirTest(){
         terminal.setCommandString("chdir");
-        terminal.setCurrentFolder(subfolder);
+        terminal.setCurrentFolder(subFolder2);
         terminal.theCommandLoop();
         Command command = terminal.getCurrentCommand();
         String correctPath = ((CommandPwdChdir) command).getPath();
-        String path = "/homeFolder/subFolder";
+        String path = "/homeFolder/subFolder1/subFolder2";
+        assertEquals(path, correctPath);
+    }
+
+    @Test
+    public void PwdTest(){
+        terminal.setCommandString("pwd");
+        terminal.setCurrentFolder(subFolder1);
+        terminal.theCommandLoop();
+        Command command = terminal.getCurrentCommand();
+        String correctPath = ((CommandPwdChdir) command).getPath();
+        String path = "/homeFolder/subFolder1";
         assertEquals(path, correctPath);
     }
 }
