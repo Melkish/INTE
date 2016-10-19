53b51c2eb4e9e49af94034eca4c18f4df6ae52da
Olivia Follin <odm.follin@gmail.com>
2016-10-19 13:27:26 +0200

    Test class for commandDirLs complemented.

--- src/main/java/se/inte/CommandDirLs.java
+++ src/main/java/se/inte/CommandDirLs.java
@@ -2,31 +2,63 @@ package se.inte;
 
 import java.util.ArrayList;
 
-public class CommandDirLs extends Command{
+public class CommandDirLs extends Command {
 
     private Folder currentFolder;
     private ArrayList printedSubFolders = new ArrayList<Folder>();
+    private ArrayList printedFiles = new ArrayList<File>();
 
-    public void execute(Terminal terminal){
+    public void execute(Terminal terminal) {
         this.currentFolder = terminal.getCurrentFolder();
 
-        if (terminal.getCommandString().startsWith("ls -a") || terminal.getCommandString().startsWith("dir /aa")){
-
-        } else if (terminal.getCommandString().startsWith("ls -l") || terminal.getCommandString().startsWith("dir /v") || terminal.getCommandString().startsWith("dir /os")){
+        if (terminal.getCommandString().startsWith("ls -a") || terminal.getCommandString().startsWith("dir /aa")) {
+            printSubFolders();
+            printFilesIncludingHidden();
+        } else if (terminal.getCommandString().startsWith("ls -l") || terminal.getCommandString().startsWith("dir /v") || terminal.getCommandString().startsWith("dir /os")) {
+            printLongFile();
 
         } else {
-            printContentNormal();
+            printSubFolders();
+            printFiles();
         }
     }
 
-    public void printContentNormal() {
-        for(Folder f : currentFolder.listSubFolders()){
+    private void printSubFolders() {
+        for (Folder f : currentFolder.listSubFolders()) {
             System.out.println(f.getFolderName());
             printedSubFolders.add(f);
         }
     }
+    private void printFilesIncludingHidden(){
+        for(File f : currentFolder.listFiles()){
+            System.out.println(f.getFileName());
+            printedFiles.add(f);
+        }
+    }
+    private void printFiles(){
+        for(File f : currentFolder.listFiles()){
+            if(!f.isHidden()){
+                System.out.println(f.getFileName());
+                printedFiles.add(f);
+            }
+        }
+    }
 
-    public ArrayList<Folder> getPrintedSubFolderList(){
+    private  void printLongFile(){
+        for(File f : currentFolder.listFiles()){
+            if(!f.isHidden()){
+                System.out.println(f.getSize() + " " + f.getFileName());
+                printedFiles.add(f);
+            }
+        }
+    }
+    public ArrayList<Folder> getPrintedSubFolderList() {
         return printedSubFolders;
     }
+
+
+    public ArrayList<File> getPrintedFileList() {
+        return printedFiles;
+    }
+
 }
